c72dec907a22d58eb586f21adeecd7be9d0f22fd
Salman-sajib <sfsajib@gmail.com>
2024-02-17 21:44:33 +0600

    added the functionality to generate a random hex color

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,34 @@ function App() {
   const [typeOfColor, setTypeOfColor] = useState('hex');
   const [color, setColor] = useState('#252525');
 
-  function handleCreateRandomHexColor() {}
+  function handleCreateRandomHexColor() {
+    const hexValues = [
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+    ];
+    let poundSign = '#';
+
+    for (let i = 0; i < 6; i++) {
+      poundSign += hexValues[Math.floor(Math.random() * hexValues.length)];
+    }
+
+    // console.log(poundSign);
+    setColor(poundSign);
+  }
 
   function handleCreateRandomRgbColor() {}
 
@@ -15,6 +42,9 @@ function App() {
         background: color,
       }}
     >
+      <h1 className='title'>
+        Click on any of the button&apos;s to generate a random color
+      </h1>
       <button onClick={() => setColor('hex')}>Generate HEX Color</button>
       <button onClick={() => setColor('rgb')}>Generate RGB Color</button>
       <button
