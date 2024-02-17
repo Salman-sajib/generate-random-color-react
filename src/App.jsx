a44cd19579d2b9d8ba05fc45d78b8719533d5012
Salman-sajib <sfsajib@gmail.com>
2024-02-17 23:03:34 +0600

    added a function for generating a random number, addedthe functionality to generate a random rgb color, added a div to show the color name

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
 
 function App() {
   const [typeOfColor, setTypeOfColor] = useState('hex');
   const [color, setColor] = useState('#252525');
+
+  function generateRandomNumber(length) {
+    return Math.floor(Math.random() * length);
+  }
 
   function handleCreateRandomHexColor() {
     const hexValues = [
@@ -23,17 +28,28 @@ function App() {
       'E',
       'F',
     ];
-    let poundSign = '#';
+    let hexColor = '#';
 
     for (let i = 0; i < 6; i++) {
-      poundSign += hexValues[Math.floor(Math.random() * hexValues.length)];
+      hexColor += hexValues[generateRandomNumber(hexValues.length)];
     }
 
     // console.log(poundSign);
-    setColor(poundSign);
+    setColor(hexColor);
   }
 
-  function handleCreateRandomRgbColor() {}
+  function handleCreateRandomRgbColor() {
+    const r = generateRandomNumber(256);
+    const g = generateRandomNumber(256);
+    const b = generateRandomNumber(256);
+
+    setColor(`rgb(${r}, ${g}, ${b})`);
+  }
+
+  useEffect(() => {
+    if (typeOfColor === 'rgb') handleCreateRandomRgbColor;
+    else handleCreateRandomHexColor;
+  }, [typeOfColor]);
 
   return (
     <div
@@ -45,8 +61,8 @@ function App() {
       <h1 className='title'>
         Click on any of the button&apos;s to generate a random color
       </h1>
-      <button onClick={() => setColor('hex')}>Generate HEX Color</button>
-      <button onClick={() => setColor('rgb')}>Generate RGB Color</button>
+      <button onClick={() => setTypeOfColor('hex')}>Generate HEX Color</button>
+      <button onClick={() => setTypeOfColor('rgb')}>Generate RGB Color</button>
       <button
         onClick={
           typeOfColor === 'hex'
@@ -56,6 +72,10 @@ function App() {
       >
         Generate Random Color
       </button>
+      <div className='color-name'>
+        <h3> {typeOfColor === 'rgb' ? 'RGB Color' : 'HEX Color'} </h3>
+        <h1> {color} </h1>
+      </div>
     </div>
   );
 }
